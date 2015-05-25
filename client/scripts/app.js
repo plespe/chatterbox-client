e5d3ed570890b50370dc93ec0d1b93107c574477
plespe <peterlucasespe@gmail.com>
2015-05-25 14:58:58 -0700

    Install bower components, create config file, instantiate app object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,54 @@
 // YOUR CODE HERE:
 
+// $.ajax({
+//   url: 'https://api.parse.com/1/classes/chatterbox',
+//   type: '',
+//   dataType: 'jsonp',
+//   success: ,
+//   error:
+// });
+
+// var message = {
+//   'username': '',
+//   'text': '',
+//   'roomname':
+// };
+var app = {
+
+  server: "hi",
+
+  init: function() {
+
+  },
+  // message should adhere to above format
+  send: function(message) {
+
+  },
+
+  fetch: function() {
+
+  },
+
+  // message should adhere to above format
+  addMessage: function(message) {
+
+  },
+
+  addFriend: function() {
+
+  },
+
+  clearMessages: function() {
+
+  },
+
+  addRoom: function() {
+
+  },
+
+  handleSubmit: function() {
+
+  }
+
+
+};
